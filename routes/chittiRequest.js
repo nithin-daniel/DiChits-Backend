0e6e75b8b38ad7c6f26cd7f44fe5638225bd0a17
Nithin Daniel <nithindaniel2018@gmail.com>
2024-07-07 11:00:33 +0530

    feat:added chitti request list

--- routes/chittiRequest.js
+++ routes/chittiRequest.js
@@ -6,8 +6,28 @@ const router = express.Router()
 
 const mongoose = require('mongoose');
 const chittiRequest = require('../models/chittiRequest');
+const Chittis = require('../models/Chittis');
 
 
+router.get('/', async (req, res) => {
+    const { chittiId } = req.body;
+
+    try {
+        const chitti = await chittiRequest.find({ chitti_id: chittiId })
+        if (chitti.length === 0) {
+            return res.status(404).json({ message: "Chitti not found" });
+
+        }
+
+        return res.status(200).json({
+            status: 200,
+            message: 'Chitti request list successfully',
+            data:chitti
+        });
+    } catch (error) {
+        return res.status(400).json({ message: error.message });
+    }
+});
 
 router.post('/add', async (req, res) => {
     const { chitti_id, user_id } = req.body;
